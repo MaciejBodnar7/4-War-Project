0b06099e596e540cb0cc98b6754ffbccf15a9121
MaciejBodnar7 <maciej.bodnar2@gmail.com>
2024-05-02 19:02:50 +0200

    new render way, determine winning card logic

--- dist/main.js
+++ dist/main.js
@@ -13,7 +13,7 @@ const me = document.getElementById("cards-value-me")
 const comp = document.getElementById("cards-value-computer")
 
 document.addEventListener("click", function (e) {
-  console.log(e.target.id) //Console log
+  // console.log(e.target.id) //Console log
   if (e.target.id === "new-card-deck-btn") {
     if (!deckId) {
       drawBtn.disabled
@@ -49,16 +49,48 @@ const drawCards = item => {
 }
 
 const render = item => {
+  const firstDiv = document.getElementById("firstDiv")
+  const secondDiv = document.getElementById("secondDiv")
+  const whoWin = document.getElementById("who-win")
   //render cards from drawCards
-  const postArr = item
-    .map(items => {
-      return `
-        <div>
-          <img class="imgs" src="${items.image}" alt="card">
-          <p>${items.value}</p>
-        </div>
-    `
-    })
-    .join("")
-  images.innerHTML = postArr
+
+  // console.log(item)
+  firstDiv.innerHTML = `
+  <p><span class="text-weight">Me: </span>${item[0].value}</p>
+  <img class="imgs" src="${item[0].image}" alt="">
+  `
+  secondDiv.innerHTML = `
+  <img class="imgs" src="${item[1].image}" alt="">
+  <p><span class="text-weight">Computer: </span>${item[1].value}</p>
+  `
+
+  whoWin.innerHTML = `<h2>${winnerOrNot(item)}</h2>`
+}
+
+const winnerOrNot = item => {
+  const valueMap = {
+    ACE: "1",
+    JACK: "11",
+    QUEEN: "12",
+    KING: "13",
+    JOKER: "14",
+  }
+
+  for (let i = 0; i < item.length; i++) {
+    const value = item[i].value
+    if (value in valueMap) {
+      item[i].value = valueMap[value]
+    }
+  }
+
+  let valueOne = Number(item[0].value)
+  let valueTwo = Number(item[1].value)
+
+  if (valueOne > valueTwo) {
+    return "You win"
+  } else if (valueOne < valueTwo) {
+    return "Computer win"
+  } else if (valueOne == valueTwo) {
+    return "tie"
+  }
 }
